8e92a65701e44d7af1090524d8c6a13bb0294b01
Nithish kumar <nithishkumarbcsmca@gmail.com>
2026-04-30 10:08:38 +0530

    Starting-of-advancedjs-1

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 // var let const
 
-var star = document.getElementsByClassName("star");
+const num_rows = document.getElementById("num-rows");
 
+num_rows.addEventListener('input',()=>{
+    const star = document.getElementsByClassName("star");
 
-
-var n = 10;
-
-for(var i = 0 ; i < n ; i++){
-    star[0].innerHTML += "<p>";
-    for(var j = 0 ; j < n; j++){
+    const n = parseInt(document.getElementById("num-rows").value);
+    star[0].innerHTML = "";
+    
+    for(let i = 0 ; i < n ; i++){
+    star[0].innerHTML += "<li style='margin:0;'>";
+    for(var j = 0 ; j <= i; j++){
        star[0].innerHTML += "*";
     }
-    star[0].innerHTML += "</p><br>";
+    star[0].innerHTML += "</li><br>";
 }
+    
+  for(let i = 0; i < n ; i++){
+      console.log("Nithish");
+  }
+        
+});
+
+//ExPLAINING CONSTANT
+
+const i = 10;
+
+//i = 9; we will not able assign the value like this
+
+console.log(i);
+
+
+
+
+
 
